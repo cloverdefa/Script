aa834cb0c0d981707ed91bcc6b17afad493dadc7
DAST-Macbook <jackie@dast.tw>
2023-03-08 22:48:19 +0800

    修正檔案路徑

--- git-pull-mac.sh
+++ git-pull-mac.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 echo "bash pull"
-cd /Users/cloverdefa/Github/bash
+cd /Users/cloverdefa/Documents/Github/bash
 git pull
 
 echo "Containers pull"
-cd /Users/cloverdefa/Github/Containers
+cd /Users/cloverdefa/Documents/Github/Containers
 git pull
 
 echo "Rule-Sets pull"
-cd /Users/cloverdefa/Github/Rule-Sets
+cd /Users/cloverdefa/Documents/Github/Rule-Sets
 git pull
 
 echo "Service pull"
-cd /Users/cloverdefa/Github/Script
+cd /Users/cloverdefa/Documents/Github/Script
 git pull
 
 echo "ssh-config pull"
-cd /Users/cloverdefa/Github/ssh-config
+cd /Users/cloverdefa/Documents/Github/ssh-config
 git pull
 
 echo "VPN-Service"
-cd /Users/cloverdefa/Github/VPN-Service
+cd /Users/cloverdefa/Documents/Github/VPN-Service
 git pull
 
 echo "Whosis-Sayings pull"
-cd /Users/cloverdefa/Github/Whosis-Sayings
+cd /Users/cloverdefa/Documents/Github/Whosis-Sayings
 git pull
 
 echo "Pull 結束"

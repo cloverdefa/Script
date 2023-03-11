6ab0cb87cc611dcd85d0139e2e5aa36064736d8d
DAST-OfficePC <jackie@dast.tw>
2023-03-11 18:17:26 +0800

    chore: update file paths and download locations, and accept longer file names
    - Update paths for downloaded files to `OneDrive/桌面`
    - Change the download location for all repositories
    - Update the yd-dlp command to accept longer file names

--- git-pull-wsl.sh
+++ git-pull-wsl.sh
@@ -3,29 +3,36 @@
 echo "bash pull"
 cd ~/Github/bash
 git pull
+echo "更新完成"
 
 echo "Containers pull"
 cd ~/Github/Containers
 git pull
+echo "更新完成"
 
 echo "Rule-Sets pull"
 cd ~/Github/Rule-Sets
 git pull
+echo "更新完成"
 
 echo "Script pull"
 cd ~/Github/Script
 git pull
+echo "更新完成"
 
 echo "ssh pull"
 cd ~/Github/ssh
 git pull
+echo "更新完成"
 
 echo "VPN-Service"
 cd ~/Github/VPN-Service
 git pull
+echo "更新完成"
 
 echo "Whosis-Sayings pull"
 cd ~/Github/Whosis-Sayings
 git pull
+echo "更新完成"
 
 echo "Pull 結束"

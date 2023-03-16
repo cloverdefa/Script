ca88ae3bfd383563216d32ca8afca2feb6f07860
DAST-iPhone <jackie@dast.tw>
2023-03-16 10:19:11 +0800

    Add Windows Terminal script for git pull

---
+++ git-pull.ps1
@@ -0,0 +1,38 @@
+# Windows Terminall Script run git pull
+
+echo "bash pull"
+cd C:/Users/clove/Github/bash
+git pull
+echo "Git Pull結束"
+
+echo "Containers pull"
+cd C:/Users/clove/Github/Containers
+git pull
+echo "Git Pull結束"
+
+echo "Rule-Sets pull"
+cd C:/Users/clove/Github/Rule-Sets
+git pull
+echo "Git Pull結束"
+
+echo "Script pull"
+cd C:/Users/clove/Github/Script
+git pull
+echo "Git Pull結束"
+
+echo "ssh pull"
+cd C:/Users/clove/Github/ssh
+git pull
+echo "Git Pull結束"
+
+echo "VPN-Service"
+cd C:/Users/clove/Github/VPN-Service
+git pull
+echo "Git Pull結束"
+
+echo "Whosis-Sayings pull"
+cd C:/Users/clove/Github/Whosis-Sayings
+git pull
+echo "Git Pull結束"
+
+echo "Pull 結束"

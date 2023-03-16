fe16dc8f627e5623290b311e2d07dce216c9e3fd
DAST-iPhone <jackie@dast.tw>
2023-03-16 10:34:43 +0800

    Fix typo in Windows Terminal script for git pull command.

--- git-pull.ps1
+++ git-pull.ps1
@@ -1,38 +1,38 @@
-# Windows Terminall Script run git pull
+# Windows Terminal Script run git pull
 
 echo "bash pull"
 cd C:/Users/clove/Github/bash
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "Containers pull"
 cd C:/Users/clove/Github/Containers
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "Rule-Sets pull"
 cd C:/Users/clove/Github/Rule-Sets
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "Script pull"
 cd C:/Users/clove/Github/Script
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "ssh pull"
 cd C:/Users/clove/Github/ssh
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "VPN-Service"
 cd C:/Users/clove/Github/VPN-Service
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "Whosis-Sayings pull"
 cd C:/Users/clove/Github/Whosis-Sayings
 git pull
-echo "Git Pull結束"
+echo "Git Pull完成"
 
 echo "Pull 結束"

a7e5195a5b701f6f73d3a574c4b65032ffd40bf6
DAST-HomePC <jackie@dast.tw>
2023-03-16 21:44:57 +0800

    GIT 提交消息：
    ###
    文檔：重構文件命名慣例和長度限制解決方案
    
    - 添加關於文件名長度限制解決方案的注釋
    ###
    
    請注意保持原有格式並確保翻譯高水平、符合程序員共識。
    
    Signed-off-by: DAST-HomePC <jackie@dast.tw>

--- yd-office.ps1
+++ yd-office.ps1
@@ -4,3 +4,5 @@ echo "影片下載工具 yd-dlp"
 echo "輸入 Ctrl+C 取消下載"
 $Server = Read-Host -Prompt '請輸入下載影片網址'
 C:\Users\clove\Downloads\yt-dlp_win\yt-dlp.exe -P "C:\Users\clove\Desktop" "$Server" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"
+
+#若出現檔案名稱超出長度限制 將參數 -o "%(title)s.%(ext)s" 修改為 -o "%(id)s.%(ext)s"

f30713ed341e6f9bad8b010b5a2de57d8899756c
DAST-OfficePC <jackie@dast.tw>
2023-03-21 07:57:17 +0800

    重構：重構視頻 URL 輸入方式，改用使用者提示
    - 將視頻 URL 的輸入方式從 Read-Host 改為使用者提示
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- yd-office.sh
+++ yd-office.sh
@@ -2,5 +2,5 @@
 
 echo "影片下載工具 yd-dlp"
 echo "輸入 Ctrl+C 取消下載"
-$Server = Read-Host -Prompt '請輸入下載影片網址'
-~/Github/Script/yt-dlp_linux -P "/mnt/c/Users/clove/Desktop" "$Server" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"
+read -p '輸入影片網址:' domain
+~/Github/Script/yt-dlp_linux -P "/mnt/c/Users/clove/Desktop" "$domain" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"

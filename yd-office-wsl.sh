7c41a09d549cd28f15c429d8f3ebf351b886e17c
DAST-OfficePC <jackie@dast.tw>
2023-04-07 01:28:21 +0000

    ### 維護：重構影片下載過程以提升效率
    - 刪除 CodeGPT-linux-amd64 檔案
    - 將 `~/Github/Script/yt-dlp_linux` 替換為 `yt-dlp` 命令以進行影片下載
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>
    
    ###

--- yd-office-wsl.sh
+++ yd-office-wsl.sh
@@ -3,4 +3,4 @@
 echo "影片下載工具 yd-dlp"
 echo "輸入 Ctrl+C 取消下載"
 read -p '輸入影片網址:' domain
-~/Github/Script/yt-dlp_linux -P "/mnt/c/Users/clove/Desktop" "$domain" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"
+yt-dlp -P "/mnt/c/Users/clove/Desktop" "$domain" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"

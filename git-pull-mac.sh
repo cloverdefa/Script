bccfa1052fe7bf803b22dda82955c61cc252e9aa
DAST-iPhone <jackie@dast.tw>
2023-05-10 18:36:44 +0800

    Fix git branch to git checkout in pull script

--- git-pull-mac.sh
+++ git-pull-mac.sh
@@ -2,49 +2,49 @@
 
 echo "bash pull"
 cd /Users/cloverdefa/Documents/Github/bash
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Containers pull"
 cd /Users/cloverdefa/Documents/Github/Containers
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Rule-Sets pull"
 cd /Users/cloverdefa/Documents/Github/Rule-Sets
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Script pull"
 cd /Users/cloverdefa/Documents/Github/Script
 git pull
-git branch main
+git checkout main
 echo "Git Pull 完成"
 
 echo "ssh pull"
 cd /Users/cloverdefa/Documents/Github/ssh
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "VPN-Service"
 cd /Users/cloverdefa/Documents/Github/VPN-Service
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Whosis-Sayings pull"
 cd /Users/cloverdefa/Documents/Github/Whosis-Sayings
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "DnsChecker pull"
 cd /Users/cloverdefa/Documents/Github/DnsChecker
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
--- git-pull-wsl.sh
+++ git-pull-wsl.sh
@@ -2,49 +2,49 @@
 
 echo "bash pull"
 cd ~/github/bash
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Containers pull"
 cd ~/github/Containers
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Rule-Sets pull"
 cd ~/github/Rule-Sets
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Script pull"
 cd ~/github/Script
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "ssh pull"
 cd ~/github/ssh
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "VPN-Service"
 cd ~/github/VPN-Service
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "Whosis-Sayings pull"
 cd ~/github/Whosis-Sayings
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 
 echo "DnsChecker pull"
 cd ~/github/DnsChecker
-git branch main
+git checkout main
 git pull
 echo "Git Pull 完成"
 

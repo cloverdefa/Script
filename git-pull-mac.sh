e4c292a39e38a9275054340984b87e288106f832
DAST-HomePC <jackie@dast.tw>
2023-05-10 19:07:18 +0800

    chore: add completion message to git pull scripts
    - Modify git-pull-mac.sh, git-pull-wsl.sh, and git-pull.ps1 files
    - Add `Git Pull完成` echo message to the script
    
    Signed-off-by: DAST-HomePC <jackie@dast.tw>

--- git-pull-mac.sh
+++ git-pull-mac.sh
@@ -1,52 +1,76 @@
 #!/bin/bash
 
+echo "========================="
 echo "bash pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/bash
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "Containers pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/Containers
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "Rule-Sets pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/Rule-Sets
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "Script pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/Script
 git pull
 git checkout main
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "ssh pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/ssh
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "VPN-Service"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/VPN-Service
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "Whosis-Sayings pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/Whosis-Sayings
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
+echo "========================="
 echo "DnsChecker pull"
+echo "========================="
 cd /Users/cloverdefa/Documents/Github/DnsChecker
 git checkout main
 git pull
 echo "Git Pull 完成"
+echo "========================="
 
 cd /Users/cloverdefa/Documents/Github
 
--- git-pull.ps1
+++ git-pull.ps1
@@ -1,52 +1,76 @@
 # Windows Terminal Script run git pull
 
+echo "========================="
 echo "bash pull"
+echo "========================="
 z bash
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "Containers pull"
+echo "========================="
 z Containers
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "Rule-Sets pull"
+echo "========================="
 z Rule-Sets
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "Script pull"
+echo "========================="
 z Script
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "ssh pull"
+echo "========================="
 z ssh
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "VPN-Service"
+echo "========================="
 z VPN-Service
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "Whosis-Sayings pull"
+echo "========================="
 z Whosis-Sayings
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
+echo "========================="
 echo "DnsChecker pull"
+echo "========================="
 z DnsChecker
 git checkout main
 git pull
 echo "Git Pull完成"
+echo "========================="
 
 cd C:/Users/clove/Github
 

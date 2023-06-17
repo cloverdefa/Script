88fece4c75d60930f12854af4c0711635d69e0a0
HomePC-DAST <jackie@dast.tw>
2023-06-17 20:30:16 +0800

    refactor: refactor git-pull and checkout process for consistency
    - Modify git-pull.ps1 to add Get-Location commands and remove echo $(pwd)
    - Modify multiple files to checkout main and pull from remote repositories
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- git-pull.ps1
+++ git-pull.ps1
@@ -4,7 +4,7 @@ echo "========================="
 echo "bash 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\bash
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
@@ -15,89 +15,97 @@ echo "========================="
 echo "PowerShell 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\PowerShell
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "Containers 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\Containers
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "Rule-Sets 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\Rule-Sets
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "Script 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\Script
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "ssh 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\ssh
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "VPN-Service 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\VPN-Service
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 echo "========================="
 echo "Whosis-Sayings 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\Whosis-Sayings
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
 
+
 echo "========================="
 echo "DnsChecker 拉取遠端資料"
 echo "========================="
 cd c:\Users\clove\github\DnsChecker
-echo $(pwd)
+Get-Location
 git checkout main
 git fetch -p
 git pull
 echo "Git 拉取遠端資料完成"
 echo "========================="
+
 
 cd C:/Users/clove/Github
 

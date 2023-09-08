e9e25f3f09cd32848d1456f4200bd11b1f303fc1
HomePC-DAST <jackie@dast.tw>
2023-09-08 15:40:23 +0800

    refactor: refactor function arguments and remove unnecessary parentheses
    - Change the argument of the function `Git-Pull-Repo` from `bash` to `$1`
    - Remove the unnecessary parentheses around the function calls in the last section
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- git-pull-wsl.sh
+++ git-pull-wsl.sh
@@ -3,32 +3,32 @@ set -e  # 程式出錯即停止運行
 
 function Git-Pull-Repo() {
     echo "========================="
-    echo "${VMs} 拉取遠端資料"
+    echo "$1 拉取遠端資料"
     echo "========================="
-    cd ~/github/${VMs}
+    cd ~/github/$1
     echo ${PWD}
     git checkout main
     git fetch -p
     git pull
     if ( @? -ne 0 )
     then
-        echo "拉取 ${VMs} 遠端資料出現錯誤"
+        echo "拉取 $1 遠端資料出現錯誤"
     else
-        echo echo "$VMs 拉取遠端 Git 資料完成"
+        echo echo "$1 拉取遠端 Git 資料完成"
     fi
 echo "========================="
 }
 
-Git-Pull-Repo(bash) bash
-Git-Pull-Repo(Containers) Containers
-Git-Pull-Repo(hath-docker) hath-docker
-Git-Pull-Repo(PowerShell) PowerShell
-Git-Pull-Repo(python-studanty) python-studanty
-Git-Pull-Repo(Rule-Sets) Rule-Sets
-Git-Pull-Repo(Script) Script
-Git-Pull-Repo(ssh) ssh
-Git-Pull-Repo(VPN-Service) VPN-Service
-Git-Pull-Repo(Whosis-Sayings) Whosis-Sayings
+Git-Pull-Repo bash
+#Git-Pull-Repo "Containers"
+#Git-Pull-Repo "hath-docker"
+#Git-Pull-Repo "PowerShell"
+#Git-Pull-Repo "python-studanty"
+#Git-Pull-Repo "Rule-Sets"
+#Git-Pull-Repo "Script"
+#Git-Pull-Repo "ssh"
+#Git-Pull-Repo "VPN-Service"
+#Git-Pull-Repo "Whosis-Sayings"
 
 # Done
-exit 0
+exit

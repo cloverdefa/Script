eb74a05516cb770d03a5dc49f70b321308b1e006
DAST-Macbook <jackie@dast.tw>
2023-09-08 20:38:09 +0800

    refactor: refactor if statement to use one-liner syntax
    - Modify the if statement to use a one-liner syntax
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- yd-home-wsl.sh
+++ yd-home-wsl.sh
@@ -8,8 +8,7 @@ yt-dlp -P "/mnt/c/Users/clove/OneDrive/桌面" "$domain" -S "ext:mp4:m4a" -o "%(
 #若出現檔案名稱超出長度限制 將參數 -o "%(title)s.%(ext)s" 修改為 -o "%(id)s.%(ext)s"
 
 # Done
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "==== 影片下載出現錯誤 ===="
 else
     echo "==== 影片下載完成 ===="

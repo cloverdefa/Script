a94357d12b1f5ddc312c6afcce46d545e0b64c79
DAST-OfficePC <jackie@dast.tw>
2023-09-08 12:04:02 +0800

    chore: update shebangs in shell and PowerShell scripts
    - Change shebang in `git-pull-mac.sh` and `git-pull-wsl.sh` from `#!/bin/sh` to `#!/bin/bash -x`
    - Change shebang in `show-df.ps1` and `show-df.sh` from `#!/bin/sh` to `#!/bin/bash -x`
    - Remove the line `exit 0` in `show-df.sh`
    - Remove the line `exit 0` in `update-vm.sh`
    - Remove the line `exit 0` in `yd-home-wsl.sh`
    - Remove the line `exit 0` in `yd-mac.sh`
    - Remove the line `exit 0` in `yd-office-wsl.sh`
    - Remove the line `exit 0` in `yd-office.ps1`
    - Remove the line `exit 0` in `yd.ps1`
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- yd-mac.sh
+++ yd-mac.sh
@@ -6,4 +6,8 @@ yt-dlp -P "/Users/cloverdefa/Desktop" "$domain" -S "ext:mp4:m4a" -o "%(title)s.%
 
 #若出現檔案名稱超出長度限制 將參數 -o "%(title)s.%(ext)s" 修改為 -o "%(id)s.%(ext)s"
 
-exit 0
+# Done
+if [ @? -ne 0 ]; then
+    echo "======下載完成======"
+    exit
+fi

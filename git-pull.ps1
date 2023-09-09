7d4ce94a90f209d7fa1421cf5698d7f5dc93f314
HomePC-DAST <jackie@dast.tw>
2023-09-10 02:10:21 +0800

    chore: update repository path handling
    - Increase the number of repositories to be checked from 10 to 11
    - Update the local repository root path
    - Build the complete local repository path
    - Change the directory to the local repository path
    - Remove the code to change the directory to the repository path
    - Change the code to change the directory to the local repository root path
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- git-pull.ps1
+++ git-pull.ps1
@@ -1,13 +1,19 @@
 # 定義要檢查的儲存庫列表
-$repositories = @("bash", "Containers", "hath-docker", "PowerShell", "python-study",
-     "Rule-Sets", "Script", "ssh", "VPN-Service", "Whosis-Sayings")
+$repositories = @("bash", "Containers", "hath-docker", "PowerShell", 
+    "python-study", "Rule-Sets", "Script", "ssh", "VPN-Service", "Whosis-Sayings")
+
+# 新的本地存儲庫根目錄路徑
+$localRepositoryRoot = "C:\Users\clove\github"
 
 # 遍歷每個儲存庫並執行Git操作
 foreach ($repository in $repositories) {
     Write-Host "檢查儲存庫: $repository"
     
+    # 構建完整的本地存儲庫路徑
+    $localRepositoryPath = Join-Path -Path $localRepositoryRoot -ChildPath $repository
+    
     # 進入儲存庫目錄
-    Set-Location -Path $repository
+    Set-Location -Path $localRepositoryPath
     
     # 檢查是否需要切換到main分支
     $currentBranch = git rev-parse --abbrev-ref HEAD
@@ -30,7 +36,7 @@ foreach ($repository in $repositories) {
     }
     
     # 返回上一級目錄
-    Set-Location -Path ..
+    Set-Location -Path $localRepositoryRoot
     
     Write-Host "---------------------------------------------"
 }

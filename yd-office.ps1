13dd14bbb327449f32070ba2891fc980d53dbc2e
HomePC-DAST <jackie@dast.tw>
2023-09-09 15:00:23 +0800

    chore: update file paths for yt-dlp
    - Change the file path to use a different directory for yt-dlp
    - Update the file path for yt-dlp.exe
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- yd-office.ps1
+++ yd-office.ps1
@@ -3,7 +3,7 @@
 Write-Output "影片下載工具 yd-dlp"
 Write-Output "輸入 Ctrl+C 取消下載"
 $Server = Read-Host -Prompt '請輸入下載影片網址'
-$Env:USERPROFILE\OneDrive\文件\yt-dlp_win\yt-dlp.exe -P "$Env:USERPROFILE\Desktop" "$Server" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"
+C:\Users\clove\OneDrive\文件\yt-dlp\yt-dlp.exe -P C:\Users\clove\Desktop "$Server" -S "ext:mp4:m4a" -o "%(title)s.%(ext)s"
 
 #若出現檔案名稱超出長度限制 將參數 -o "%(title)s.%(ext)s" 修改為 -o "%(id)s.%(ext)s"
 

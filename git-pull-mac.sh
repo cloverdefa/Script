b062e88196213ac80f2c04e16377be8cc24be348
DAST-Macbook <jackie@dast.tw>
2023-09-09 23:42:51 +0800

    chore: refactor repository directory handling
    - Change the path to the repository directory to use the $HOME environment variable
    - Switch to the repository directory
    - Switch to the main branch
    - If the remote repository is behind, pull the changes
    - If the remote repository is not behind, output a message indicating no updates are needed
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- git-pull-mac.sh
+++ git-pull-mac.sh
@@ -9,13 +9,18 @@ function Git-Pull-Repo() {
     local repo_path="$HOME/Documents/github/$repo_name" # 使用$HOME環境變數
     local text="$repo_name 拉取遠端資料"
 
-    cd "$repo_path"
-    echo "目前路徑位於 ${PWD}"
+    cd "$repo_path" # 切換到存儲庫目錄
 
-    if git checkout main && git fetch -p && git pull; then
-        echo "拉取 $repo_name 遠端資料完成"
+    git checkout main # 切換到主分支
+
+    if git remote update -p && git status -uno | grep -q 'Your branch is behind'; then
+        if git pull; then
+            echo "拉取 $repo_name 遠端資料完成"
+        else
+            echo "拉取 $repo_name 遠端資料出現錯誤"
+        fi
     else
-        echo "拉取 $repo_name 遠端資料出現錯誤"
+        echo "GitHub 遠端資料庫無變更或本地資料不需要更新，存儲庫名稱：$repo_name"
     fi
 }
 

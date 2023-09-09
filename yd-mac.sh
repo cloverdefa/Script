4b456c5fefd9d33d19ca94db8746eaa90b3c6417
HomePC-DAST <jackie@dast.tw>
2023-09-09 19:58:14 +0800

    The best label for this commit would be "refactor". The changes made in the files are not fixing a bug or adding a new feature, but rather improving the code by modifying the scripts to use the `yt-dlp` command in a single line and adding a completion message.: consolidate yt-dlp command usage in script files
    - Modify the `yd-home-wsl.sh` script to use the `yt-dlp` command in a single line and print a completion message
    - Modify the `yd-mac.sh` script to use the `yt-dlp` command in a single line and print a completion message
    - Modify the `yd-office-wsl.sh` script to use the `yt-dlp` command in a single line and print a completion message
    - Modify the `yd-office.ps1` script to use the `yt-dlp` command in a single line and print a completion message
    - Modify the `yd.ps1` script to use the `yt-dlp` command in a single line and print a completion message
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- yd-mac.sh
+++ yd-mac.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
 output_dir="/Users/cloverdefa/Desktop"
 format="ext:mp4:m4a"
 
 echo "影片下載工具 yd-dlp"
-echo "輸入 Ctrl+C 取消下載"
 read -p '輸入影片網址: ' domain
 
-yt-dlp -P "$output_dir" "$domain" -S "$format" -o "%(title)s.%(ext)s"
-
-if [ $? -ne 0 ]; then
-    echo "==== 影片下載出現錯誤 ===="
-else
+if yt-dlp -P "$output_dir" "$domain" -S "$format" -o "%(title)s.%(ext)s"; then
     echo "==== 影片下載完成 ===="
+else
+    echo "==== 影片下載出現錯誤 ===="
 fi
 
 exit
--- yd.ps1
+++ yd.ps1
@@ -4,20 +4,11 @@ Write-Output "影片下載工具 yd-dlp"
 Write-Output "輸入 Ctrl+C 取消下載"
 $Video = Read-Host -Prompt '請輸入下載影片網址'
 
-# 設定 yt-dlp.exe 的路徑
 $ytDlpPath = "C:\Users\clove\OneDrive\文件\yt-dlp\yt-dlp.exe"
-
-# 設定下載目錄為桌面
 $outputDir = "C:\Users\clove\OneDrive\桌面"
 $format = "ext:mp4:m4a"
 
-# 使用 yt-dlp 下載
 & $ytDlpPath -P $outputDir "$Video" -S $format -o "%(title)s.%(ext)s"
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Output "影片下載完成"
-} else {
-    Write-Output "影片下載出現錯誤"
-}
-
+$downloadStatus = if ($LASTEXITCODE -eq 0) { "完成" } else { "出現錯誤" }
+Write-Output "影片下載$downloadStatus"
 Exit $LASTEXITCODE

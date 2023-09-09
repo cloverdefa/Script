eea4a7083600726f4268a616bb667530af024411
HomePC-DAST <jackie@dast.tw>
2023-09-10 01:20:55 +0800

    refactor: refactor ping and df commands in shell script
    - Remove the unnecessary ping check before running the ssh command
    - Remove the else block after the ping check
    - Remove the sleep command after running the df command
    - Change the condition for the "Done" message from `$?` to `$LASTEXITCODE -eq 0`
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- show-df.ps1
+++ show-df.ps1
@@ -12,13 +12,9 @@ function Show-DiskSpace {
     Write-Output ("=" * 25)
 
     try {
-        $pingResult = Test-Connection -ComputerName $server -Count 1 -ErrorAction Stop
-
-        if ($pingResult.StatusCode -eq 0) {
-            Write-Output "$server 伺服器不存在"
-        } else {
-            ssh "$server" 'LC_ALL=C df -h'
-        }
+        $dfOutput = ssh $server 'LC_ALL=C df -h'
+        $dfOutput | ForEach-Object { Write-Output $_ }
+        Start-Sleep -Seconds 2
     } catch {
         Write-Output "讀取 $server 空間出現錯誤: $_"
     }
@@ -29,7 +25,7 @@ foreach ($server in $servers) {
 }
 
 # Done
-if ($?) {
+if ($LASTEXITCODE -eq 0) {
     Write-Output "讀取空間資料完成"
 } else {
     Write-Output "讀取空間出現錯誤"

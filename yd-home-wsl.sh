39b058ff87c0fcf307cd1564f1c7bc0e688386c1
HomePC-DAST <jackie@dast.tw>
2023-09-09 22:09:01 +0800

    feat: improve error handling and output in download script
    - Change the value of `output_dir` to `/mnt/c/Users/clove/OneDrive/桌面`
    - Change the value of `format` to `ext:mp4:m4a`
    - Add comments explaining the purpose of the script
    - Redirect the stderr output of `yt-dlp` to the `error_msg` variable
    - Store the exit code of `yt-dlp` in the `exit_code` variable
    - Add conditions to handle different exit codes and display appropriate error messages
    - Print the `error_msg` if there was an error during the download
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- yd-home-wsl.sh
+++ yd-home-wsl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+output_dir="/mnt/c/Users/clove/OneDrive/桌面"
+format="ext:mp4:m4a"
+
 echo "影片下載工具 yd-dlp"
 echo "輸入 Ctrl+C 取消下載"
 read -p '輸入影片網址: ' domain
 
-output_dir="/mnt/c/Users/clove/OneDrive/桌面"
-format="ext:mp4:m4a"
-
-# 使用 yt-dlp 命令下載影片
-yt-dlp -P "$output_dir" "$domain" -S "$format" -o "%(title)s.%(ext)s"
+# 使用 yt-dlp 命令下載影片，並將 stderr 重定向到 error_msg 變數
+error_msg=$(yt-dlp -P "$output_dir" "$domain" -S "$format" -o "%(title)s.%(ext)s" 2>&1)
 exit_code=$?
 
 if [ $exit_code -eq 0 ]; then
@@ -21,6 +21,8 @@ else
     else
         echo "==== 未知錯誤，退出代碼：$exit_code ===="
     fi
+    echo "錯誤訊息："
+    echo "$error_msg"
 fi
 
-exit
+exit $exit_code

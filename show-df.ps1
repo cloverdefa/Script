e20f2750398176149ffad0c9933cbbc83125b4bc
HomePC-DAST <jackie@dast.tw>
2023-09-09 16:29:52 +0800

    refactor: refactor disk space information display
    - Remove the requirement for PowerShell version 2
    - Change `Write-Host` to `Write-Output` for consistent output handling
    - Add blank lines for better readability
    - Catch and handle errors when reading disk space information
    - Remove the delay before completing the function
    - Iterate over the list of servers to display disk space information for each one
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- show-df.ps1
+++ show-df.ps1
@@ -1,30 +1,34 @@
-#REQUIRES -Version 2
-
 function Show-DiskSpace {
     param (
         [string]$server
     )
 
-    Write-Host "========================="
-    Write-Host "$server 容量空間"
-    Write-Host "========================`n"
-    
+    Write-Output "========================="
+    Write-Output "    $server 容量空間"
+    Write-Output "========================="
+    Write-Output ""
+
     try {
         $dfOutput = ssh $server 'df -h'
-        Write-Host $dfOutput
-        Write-Host "`n讀取空間資料完成"
+        Write-Output $dfOutput
+        Write-Output ""
+        Start-Sleep -Seconds 2
     } catch {
-        Write-Host "讀取 $server 空間出現錯誤: $_"
+        Write-Output "讀取 $server 空間出現錯誤: $_"
     }
-
-    Start-Sleep -Seconds 2
 }
 
 $servers = "np", "zt", "agh", "up", "wp", "rd", "n1", "n2", "n3", "n4"
 
-$servers | ForEach-Object {
-    Show-DiskSpace -server $_
+foreach ($server in $servers) {
+    Show-DiskSpace -server $server
 }
 
 # Done
+if ($?) {
+    Write-Output "讀取空間資料完成"
+} else {
+    Write-Output "讀取空間出現錯誤"
+}
+
 Exit

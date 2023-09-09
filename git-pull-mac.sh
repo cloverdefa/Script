72a2b360a9f161770616d3187a7d526c075e6ded
DAST-Macbook <jackie@dast.tw>
2023-09-09 20:35:41 +0800

    refactor: refactor file handling and error handling
    - Remove the commented out line of equals signs
    - Move the `repo_path` variable assignment outside the function
    - Remove the unnecessary `cd` command
    - Simplify the error handling in the `if` statement
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- git-pull-mac.sh
+++ git-pull-mac.sh
@@ -1,22 +1,24 @@
 #!/bin/zsh
 
+# 函數來執行Git操作
 function Git-Pull-Repo() {
     local repo_name="$1"
-    local equals="="*25 # 產生等號（=）的25個重複字符
-    echo "$equals"
+    local repo_path="~/Documents/github/$repo_name"
+
+    echo "========================="
     echo "$repo_name 拉取遠端資料"
-    echo "$equals"
-    cd ~/Documents/github/"$repo_name"
+    echo "========================="
+
+    cd "$repo_path"
     echo "目前路徑位於 ${PWD}"
-    git checkout main
-    git fetch -p
-    git pull
-    if [ $? -ne 0 ]; then
-        echo "拉取 $repo_name 遠端資料出現錯誤"
-    else
+
+    if git checkout main && git fetch -p && git pull; then
         echo "$repo_name 拉取遠端 Git 資料完成"
+    else
+        echo "拉取 $repo_name 遠端資料出現錯誤"
     fi
-    echo "$equals"
+
+    echo "========================="
 }
 
 repos=("bash" "Containers" "hath-docker" "PowerShell" "python-study" "Rule-Sets" "Script" "ssh" "VPN-Service" "Whosis-Sayings")
@@ -25,5 +27,5 @@ for repo in "${repos[@]}"; do
     Git-Pull-Repo "$repo"
 done
 
-# Done
+# 完成
 exit

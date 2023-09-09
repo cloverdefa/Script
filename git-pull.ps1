3a6fd27b1b4f9dc3ccae597364af8c3fd5004dd3
HomePC-DAST <jackie@dast.tw>
2023-09-10 02:08:08 +0800

    refactor: refactor Git-Pull-Repo function for improved efficiency
    - Remove the `$reposToPull` array and replace it with a hardcoded list
    - Change the function name from `Git-Pull-Repo` to `Git-Pull-Repository`
    - Remove the `Write-Host` statement for printing the repository path
    - Replace the hardcoded repository list with a defined list
    - Loop through each repository and perform Git operations
    - Add a prompt to switch to the `main` branch if the current branch is not `main`
    - Remove the `force` parameter and the related logic
    - Replace the `git remote update -p` command with `git pull` to fetch the latest remote branch
    - Check if the repository is up to date or has untracked files after pulling
    - Print appropriate messages for different synchronization states
    - Return to the parent directory after each repository operation
    - Add a separator line after each repository operation
    - Remove the `Set-Location $Env:USERPROFILE` and `Exit` statements
    - Return to the original directory using `Set-Location -Path $PSScriptRoot`
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- git-pull.ps1
+++ git-pull.ps1
@@ -1,40 +1,39 @@
-$reposToPull = @(
-    "bash", "Containers", "hath-docker", "PowerShell", "python-study",
-    "Rule-Sets", "Script", "ssh", "VPN-Service", "Whosis-Sayings"
-)
+# 定義要檢查的儲存庫列表
+$repositories = @("bash", "Containers", "hath-docker", "PowerShell", "python-study",
+     "Rule-Sets", "Script", "ssh", "VPN-Service", "Whosis-Sayings")
 
-function Git-Pull-Repo {
-    param (
-        [string]$repoName,
-        [switch]$force
-    )
-
-    $repoPath = "$Env:USERPROFILE\github\$repoName"
-    Write-Host "拉取GitHub遠端資料庫 $repoName 位於 $repoPath"
-
-    if (Test-Path -Path $repoPath) {
-        Set-Location $repoPath
+# 遍歷每個儲存庫並執行Git操作
+foreach ($repository in $repositories) {
+    Write-Host "檢查儲存庫: $repository"
+    
+    # 進入儲存庫目錄
+    Set-Location -Path $repository
+    
+    # 檢查是否需要切換到main分支
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+    if ($currentBranch -ne "main") {
+        Write-Host "切換到main分支中..."
         git checkout main
-
-        if ($force) {
-            git remote update -p
-            git status -uno | Select-String 'Your branch is behind' | Out-Null
-            if ($?) {
-                git pull
-            }
-        }
-
-        $statusMsg = if ($LASTEXITCODE -eq 0) { "成功完成" } else { "拉取失敗" }
-        Write-Host "拉取GitHub遠端資料庫 $repoName $statusMsg"
-    } else {
-        Write-Host "資料夾 $repoName 不存在"
-        return 1
     }
+    
+    # 拉取最新的遠程分支
+    git pull
+    
+    # 檢查是否同步成功
+    $result = git status
+    if ($result -match "Your branch is up to date") {
+        Write-Host "儲存庫已經是最新的。"
+    } elseif ($result -match "Untracked files present") {
+        Write-Host "儲存庫有未追蹤的檔案。請檢查。"
+    } else {
+        Write-Host "儲存庫同步失敗。請檢查是否有變更或問題。"
+    }
+    
+    # 返回上一級目錄
+    Set-Location -Path ..
+    
+    Write-Host "---------------------------------------------"
 }
 
-$reposToPull | ForEach-Object {
-    Git-Pull-Repo -repoName $_ -force
-}
-
-Set-Location $Env:USERPROFILE
-Exit
+# 返回到原始目錄
+Set-Location -Path $PSScriptRoot

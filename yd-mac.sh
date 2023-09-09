7f5f6f5bff13456e30c3e7d9c43bfb9350f553bd
HomePC-DAST <jackie@dast.tw>
2023-09-09 22:01:46 +0800

    feat: switch to `yt-dlp` for video downloads and improve error handling
    - Modify the script to use `yt-dlp` instead of `youtube-dl` for downloading videos
    - Add error handling for different exit codes
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- yd-mac.sh
+++ yd-mac.sh
@@ -6,10 +6,18 @@ format="ext:mp4:m4a"
 echo "影片下載工具 yd-dlp"
 read -p '輸入影片網址: ' domain
 
+# 使用 yt-dlp 命令下載影片
 if yt-dlp -P "$output_dir" "$domain" -S "$format" -o "%(title)s.%(ext)s"; then
     echo "==== 影片下載完成 ===="
 else
-    echo "==== 影片下載出現錯誤 ===="
+    exit_code=$?
+    if [ $exit_code -eq 1 ]; then
+        echo "==== 影片下載過程中出現錯誤 ===="
+    elif [ $exit_code -eq 2 ]; then
+        echo "==== 輸入的網址無效 ===="
+    else
+        echo "==== 未知錯誤，退出代碼：$exit_code ===="
+    fi
 fi
 
 exit

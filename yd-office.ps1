bc4a008d92922c3b082a4df85955daf87b9ce949
DAST-OfficePC <jackie@dast.tw>
2023-09-11 12:13:55 +0800

    feat: refactor video download functionality
    - Set the output directory to the desktop
    - Set the video format to "ext:mp4:m4a"
    - Set the path to yt-dlp.exe
    - Display prompt messages
    - Read input for video URL
    - Build yt-dlp command string
    - Execute yt-dlp command
    - Exit the program
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- yd-office.ps1
+++ yd-office.ps1
@@ -1,16 +1,28 @@
-#REQUIRES -Version 6
+# 設定輸出目錄為桌面
+$outputDir = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop')
 
-"影片下載工具 yd-dlp", "輸入 Ctrl+C 取消下載" | Write-Host
-$Server = Read-Host -Prompt '請輸入下載影片網址'
-$outputDir = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop')  # 使用環境變數來設定輸出目錄
+# 設定影片格式
 $format = "ext:mp4:m4a"
-$ytDlpPath = [System.IO.Path]::Combine($env:USERPROFILE, 'OneDrive', '文件', 'yt-dlp', 'yt-dlp.exe')  # 使用環境變數設定 yt-dlp 路徑
 
+# 設定 yt-dlp 的路徑
+$ytDlpPath = [System.IO.Path]::Combine($env:USERPROFILE, 'OneDrive', '文件', 'yt-dlp', 'yt-dlp.exe')
+
+# 顯示提示信息
+"影片下載工具 yd-dlp"
+"輸入 Ctrl+C 取消下載" | Write-Host
+
+# 輸入影片網址
+$Server = Read-Host -Prompt '請輸入下載影片網址'
+
+# 建立 yt-dlp 命令字串
 $command = "$ytDlpPath -P `"$outputDir`" `"$Server`" -S $format -o `"%(title)s.%(ext)s`""
+
+# 執行 yt-dlp 命令
 if (Invoke-Expression $command) {
     "影片下載完成" | Write-Host
 } else {
     "影片下載出現錯誤" | Write-Host
 }
 
+# 結束程式
 Exit

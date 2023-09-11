22e61e782bf4e69b85276f315eb3f673b292e4e5
DAST-OfficePC <jackie@dast.tw>
2023-09-11 12:24:35 +0800

    The label best describing this change is "feat".: improve error handling and command line options for `ytDlpPath` command
    - Modify the path for `ytDlpPath`
    - Add a command line option `--newline --progress` to the `ytDlpPath` command
    - Add a try-catch block to handle errors during video downloads
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- yd-office.ps1
+++ yd-office.ps1
@@ -5,7 +5,10 @@ $outputDir = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop')
 $format = "ext:mp4:m4a"
 
 # 設定 yt-dlp 的路徑
-$ytDlpPath = [System.IO.Path]::Combine($env:USERPROFILE, 'OneDrive', '文件', 'yt-dlp', 'yt-dlp.exe')
+$ytDlpPath = [System.IO.Path]::Combine(
+    $env:USERPROFILE, 'OneDrive', '文件', 'yt-dlp', 'yt-dlp.exe')
+$ytDlpPath = [System.IO.Path]::Combine(
+    $env:USERPROFILE, 'OneDrive', '文件', 'yt-dlp','yt-dlp.exe')
 
 # 顯示提示信息
 "影片下載工具 yd-dlp"
@@ -15,12 +18,13 @@ $ytDlpPath = [System.IO.Path]::Combine($env:USERPROFILE, 'OneDrive', '文件', '
 $Server = Read-Host -Prompt '請輸入下載影片網址'
 
 # 建立 yt-dlp 命令字串
-$command = "$ytDlpPath -P `"$outputDir`" `"$Server`" -S $format -o `"%(title)s.%(ext)s`""
+$command = "$ytDlpPath -P `"$outputDir`" `"$Server`" -S $format -o `"%(title)s.%(ext)s`" --newline --progress"
 
-# 執行 yt-dlp 命令
-if (Invoke-Expression $command) {
+# 開始 yt-dlp 進程並顯示輸出
+try {
+    Invoke-Expression $command
     "影片下載完成" | Write-Host
-} else {
+} catch {
     "影片下載出現錯誤" | Write-Host
 }
 

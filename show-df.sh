0ae0772a85f7acb8e296c546b210d36eedf305c6
DAST-OfficePC <jackie@dast.tw>
2023-09-13 15:12:49 +0800

    功能：更新磁盤空間顯示和伺服器清單處理
    - 將 `Show-DiskSpace` 函數的行範圍從 `1,39` 增加到 `1,51`
    - 新增要查詢磁盤空間的伺服器名稱清單
    - 新增顯示磁盤空間的函數
    - 在顯示伺服器磁盤空間之前加入分隔線
    - 新增顯示伺服器磁盤空間的標題
    - 在顯示伺服器磁盤空間之後加入分隔線
    - 將SSH命令的連接超時設置為5秒
    - 如果伺服器不存在，顯示一則訊息
    - 顯示 `df` 命令的輸出
    - 在顯示 `df` 輸出後暫停2秒
    - 如果伺服器不存在或連接超時，顯示一則訊息
    - 遍歷伺服器清單並

--- show-df.sh
+++ show-df.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# 定義要查詢的伺服器名稱列表
 servers=("np" "zt" "agh" "up" "wp"
      "rd" "n1" "n2" "n3" "n4")
 
+# 定義顯示磁碟空間的函數
 function Show-DiskSpace {
     local server="$1"
 
+    # 顯示分隔線
     echo "========================="
+    
+    # 顯示伺服器容量空間的標題
     echo "    $server 容量空間"
+    
+    # 顯示分隔線
     echo "========================="
 
     # 使用 -o ConnectTimeout 設置連接超時為5秒
@@ -15,25 +22,30 @@ function Show-DiskSpace {
         dfOutput=$(ssh "$server" 'LC_ALL=C df -h')
 
         if [[ "$dfOutput" == *"No route to host"* ]]; then
+            # 顯示伺服器不存在的訊息
             echo "$server 伺服器不存在"
         else
+            # 顯示 df 命令輸出
             echo "$dfOutput"
             sleep 2
         fi
     else
+        # 顯示伺服器不存在或連接超時的訊息
         echo "$server 伺服器不存在或連接超時"
     fi
 }
 
+# 遍歷伺服器列表並呼叫 Show-DiskSpace 函數以顯示磁碟空間
 for server in "${servers[@]}"; do
     Show-DiskSpace "$server"
 done
 
-# Done
+# 完成訊息
 if [ $? -ne 0 ]; then
     echo "讀取空間出現錯誤"
 else
     echo "讀取空間資料完成"
 fi
 
+# 結束程式
 exit

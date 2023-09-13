d81b2421d97dae3cabe35bfad1db372368e91f8b
DAST-OfficePC <jackie@dast.tw>
2023-09-13 15:20:46 +0800

    功能：重構腳本以更新多個虛擬機器
    - 更新腳本以包含要更新的主機名稱列表
    - 定義一個函數以更新虛擬機器
    - 添加SSH命令以執行虛擬機器更新操作
    - 遍歷主機名稱列表並為每個主機調用更新函數
    - 刪除“完成”註釋
    - 在更新所有設備後顯示完成消息
    - 在腳本結束時添加退出命令

--- update-vm.sh
+++ update-vm.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+# 定義要更新的主機名稱列表
 hostnames=("pve" "np" "zt" "agh" "bot" "up" "wp"
     "rd" "n1" "n2" "n3" "n4" "uckh" "ucko")
 
+# 定義更新虛擬機的函數
 function Update-VM() {
     echo "======================================="
     echo "      更新 $1 主機"
     echo "======================================="
+    # 使用 SSH 命令執行虛擬機更新作業
     ssh $1 'update-vm'
     echo ""
 }
 
+# 遍歷主機名稱列表並呼叫 Update-VM 函數以執行更新作業
 for hostname in "${hostnames[@]}"; do
     Update-VM "$hostname"
 done
 
-# Done
+# 顯示完成訊息
 if [ $? -ne 0 ]; then
     echo "==== 更新 $1 出現錯誤 ===="
 else
     echo "==== 更新全部設備完成 ===="
 fi
+
+# 結束程式
 exit

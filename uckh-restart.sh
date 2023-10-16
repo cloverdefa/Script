c9f18a786b7fbcdd885dc11317fd06500bc95f50
DAST-OfficePC <jackie@dast.tw>
2023-10-17 07:43:47 +0800

    功能：實現遠程SSH配置和執行
    - 添加開關的遠程SSH配置
    - 設置遠程配置的命令
    - 在遠程設備上執行SSH命令
    - 打印完成訊息

--- uckh-restart.sh
+++ uckh-restart.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-ssh switch-core '(echo "enable" ; echo "configure" ; echo "interface 0/1" ; echo "poe opmode shutdown" ; echo  "poe opmode auto" ; echo "exit" ; echo "exit"; echo "exit"  ) | telnet localhost 23 ; exit;'
+# 遠程設備的SSH設定
+SSH_HOST="switch-core"
+SSH_USER="your_username"
+SSH_PASS="your_password"
+
+# 設置命令
+CONFIG_COMMANDS=(
+    "enable"
+    "configure"
+    "interface 0/1"
+    "poe opmode shutdown"
+    "poe opmode auto"
+    "exit"
+    "exit"
+    "exit"
+)
+
+# SSH連接和配置遠程設備
+sshpass -p "$SSH_PASS" ssh "$SSH_USER"@"$SSH_HOST" << EOF
+    for cmd in "\${CONFIG_COMMANDS[@]}"; do
+        echo "\$cmd"
+        sleep 1
+    done
+    exit
+EOF
+
+echo "Unifi Cloudkey Home 重新PoE送電啟動完成."

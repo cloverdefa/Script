d6ceba0a08d9b17573a12093673639264114426b
DAST-OfficePC <jackie@dast.tw>
2023-10-17 07:49:42 +0800

    維護：更新 SSH 配置以提升安全性
    - 在 `uckh-restart.ps1` 中修改 SSH 配置，使用變數代替硬編碼的值
    - 在 `uckh-restart.sh` 中更新 SSH 配置，改用 SSH 金鑰驗證而非密碼驗證

--- uckh-restart.sh
+++ uckh-restart.sh
@@ -2,8 +2,8 @@
 
 # 遠程設備的SSH設定
 SSH_HOST="switch-core"
-SSH_USER="your_username"
-SSH_PASS="your_password"
+SSH_USER="cloverdefa"
+SSH_KEY="~/.ssh/id_rsa"  # 私鑰文件的路徑
 
 # 設置命令
 CONFIG_COMMANDS=(
@@ -17,8 +17,8 @@ CONFIG_COMMANDS=(
     "exit"
 )
 
-# SSH連接和配置遠程設備
-sshpass -p "$SSH_PASS" ssh "$SSH_USER"@"$SSH_HOST" << EOF
+# 使用SSH金鑰認證連接和配置遠程設備
+ssh -i "$SSH_KEY" "$SSH_USER"@"$SSH_HOST" << EOF
     for cmd in "\${CONFIG_COMMANDS[@]}"; do
         echo "\$cmd"
         sleep 1

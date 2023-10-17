78f041a2a91a112b5827c1dbda775d9f9d2c5ee5
DAST-OfficePC <jackie@dast.tw>
2023-10-17 15:28:54 +0800

    功能：使用新的OMP版本更新虛擬機器
    - 新增文件`omp-update.ps1`和`omp-update.sh`
    - 定義要更新的主機名稱列表
    - 定義`Update-OMP`函數以更新虛擬機器
    - 遍歷主機名稱列表並呼叫`Update-OMP`函數
    - 顯示成功更新的完成訊息
    - 顯示失敗更新的錯誤訊息

---
+++ omp-update.ps1
@@ -0,0 +1,45 @@
+# 定義要更新的主機名稱列表
+$hostnames = "snell", "zero-trust", "agh", 
+    "rustdesk-server", "wordpress", 
+    "node-01", "node-02", "node-03", "node-04", 
+    "unifi-cloudkey-home", "unifi-cloudkey-office"
+
+# 定義更新虛擬機的函數
+function Update-OMP {
+    param (
+        [string]$hostname
+    )
+    # 顯示正在更新的主機名稱
+    Write-Host "=============================="
+    Write-Host "更新 $hostname 主機 oh my posh"
+    Write-Host "=============================="
+
+    # 使用 SSH 命令執行虛擬機更新作業
+    ssh $hostname 'ompu'
+
+    # 顯示空行
+    Write-Host ""
+}
+
+# 初始化成功標誌
+$success = $true
+
+# 遍歷主機名稱列表並呼叫 Update-VM 函數以執行更新作業，並傳遞更新命令
+$updateCommand = "ompu"  # 替換為實際的更新命令路徑
+$hostnames | ForEach-Object {
+    Update-VM -hostname $_ -updateCommand 'ompu'
+    # 檢查是否有更新錯誤，如果有，將成功標誌設置為 false
+    if ($LASTEXITCODE -ne 0) {
+        $success = $false
+    }
+}
+
+# 顯示完成訊息
+if ($success) {
+    Write-Host "全部更新作業完成"
+} else {
+    Write-Host "更新出現錯誤"
+}
+
+# 結束程式，並根據成功標誌設置退出代碼
+Exit $success

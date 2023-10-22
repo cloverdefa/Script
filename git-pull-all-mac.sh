d6187a871a49719360ffa8ba8f5e5f5963c320a4
Macbook <jackie@dast.tw>
2023-10-22 21:33:59 +0800

    重構：重構更新程序並改善錯誤處理
    - 添加一個用於檢查更新的存儲庫列表
    - 將函數 `Update-VM` 改名為 `Git-Pull-Repo`
    - 切換到主分支時修改錯誤信息
    - 添加對遠程更新的檢查，如果需要則進行拉取
    - 根據拉取結果修改完成信息

--- git-pull-all-mac.sh
+++ git-pull-all-mac.sh
@@ -1,39 +1,41 @@
 #!/bin/zsh
 
-# 從 update_server_list.txt 檔案中讀取主機名稱列表
-hostnames=("${(@f)$(<"$HOME/Documents/github/Script/update_server_list.txt")}")
+# 定義要檢查的儲存庫列表
+repos=("${(@f)$(<$HOME/Documents/github/Script/repositories.txt)}")
 
-# 定義更新虛擬機的函數
-function Update-VM {
-    local hostname="$1"
-    echo "======================================="
-    echo "      更新 $hostname 主机"
-    echo "======================================="
-    # 使用 SSH 命令执行虚拟机更新操作
-    ssh "$hostname" "update-vm"
-    local exit_code="$?"
-    if [ "$exit_code" -ne 0 ]; then
-        echo "==== 更新 $hostname 出现错误 ===="
+# 函數來執行 Git 操作
+function Git-Pull-Repo() {
+    local repo_name="$1"
+    local repo_path="$HOME/Documents/github/$repo_name" # 使用 $HOME 環境變數
+    local text="$repo_name 拉取遠端資料"
+
+    cd "$repo_path" || exit 1 # 切換到儲存庫目錄如果切換失敗，退出腳本，並返回退出碼1
+
+    # 切換到主分支
+    if git checkout main; then
+        echo "已成功切換到主分支"
     else
-        echo "==== 更新 $hostname 完成 ===="
+        echo "切換到主分支時出現錯誤"
+        exit 1 # 如果切換失敗，退出腳本，並返回退出碼1
     fi
-    echo ""
+
+    if git remote update -p && git status -uno | grep -q 'Your branch is behind'; then
+        if git pull; then
+            text="$text 完成"
+        else
+            text="$text 出現錯誤"
+        fi
+    else
+        text="GitHub 遠端資料庫無變更或本地資料不需要更新，存儲庫名稱：$repo_name"
+    fi
+
+    echo "$text"
+    echo "----------------------------------------------------------------------"
 }
 
-# 初始化成功标志
-success=true
-
-# 遍历主机名列表并调用 Update-VM 函数以执行更新操作
-for hostname in "${hostnames[@]}"; do
-    Update-VM "$hostname"
+for repo in "${repos[@]}"; do
+    Git-Pull-Repo "$repo"
 done
 
-# 显示完成消息
-if [ "$success" = true ]; then
-    echo "==== 更新全部设备完成 ===="
-else
-    echo "==== 更新出现错误 ===="
-fi
-
-# 结束脚本
-exit
+# 完成
+exit 0

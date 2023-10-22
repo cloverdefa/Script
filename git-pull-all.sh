3eeb059600b8b5705764f486a9c116ac80e1c5a3
DAST-HomePC <jackie@dast.tw>
2023-10-22 21:07:14 +0800

    事務：更新存儲庫文件的路徑和註釋
    - 將存儲庫文件的路徑更改為 `$HOME/Documents/github/repositories.txt`
    - 更新註釋以反映新的路徑

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -6,7 +6,7 @@ mapfile -t repos < $HOME/Documents/github/Script/repositories.txt
 # 函數來執行Git操作
 function Git-Pull-Repo() {
     local repo_name="$1"
-    local repo_path="$HOME/github/$repo_name" # 使用$HOME環境變數
+    local repo_path="$HOME/Documents/github/$repo_name" # 使用$HOME環境變數
     local text="$repo_name 拉取遠端資料"
 
     cd "$repo_path" || exit 1 # 切換到存儲庫目錄如果切換失敗，退出腳本，並返回退出碼1

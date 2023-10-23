ae58ba9b0ba7c21ca5bcb3437cc85641236a95c9
DAST-iPhone <jackie@dast.tw>
2023-10-23 09:10:29 +0800

    Refactor update-omp.sh to use mapfile instead of while loop

--- update-omp.sh
+++ update-omp.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # 從 .omp.list 檔案中讀取主機名單
-servers=()
-while IFS= read -r line; do
-  hostnames+=("$line")
-done < ".omp.list"
+mapfile -t hostnames < ".omp.list"
 
 # 定義更新虛擬機的函數
 function Update-OMP() {

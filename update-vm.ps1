56d36ff74d3c7f54dfc8b540b13cbc3aca808057
DAST-HomePC <jackie@dast.tw>
2023-10-25 21:26:19 +0800

    重構：重構檔案處理和腳本執行
    - 從檔案中移除註解和空行
    - 更新差異元資料中的檔案路徑
    - 將 PowerShell 腳本改用正則表達式進行篩選
    - 將 Bash 腳本改用 grep 進行篩選
    - 將 PowerShell 函數改用英文名稱
    - 將 Bash 函數改用中文名稱
    - 修正 SSH 命令以更新虛擬機
    - 執行 SSH 命令後檢查退出碼
    - 更新主機時發生錯誤時輸出訊息
    - 所有主機更新完成後輸出訊息
    - 以適當的退出碼結束腳本

--- update-vm.ps1
+++ update-vm.ps1
@@ -1,42 +1,42 @@
-# 從 .server.list 檔案中讀取主機名稱列表
-$hostnames = Get-Content -Path "$HOME\.config\list\.server.list"
+<# 從 .server.list 檔案中讀取主機名稱列表，過濾掉空白行和註釋行 #>
+$hostnames = Get-Content -Path "$HOME\.config\list\.server.list" | Where-Object { $_ -match '^\s*[^#].*' }
 
-# 定義更新虛擬機的函數
+<# 定義更新虛擬機的函數 #>
 function Update-VM {
     param (
         [string]$hostname
     )
-    # 顯示正在更新的主機名稱
+    <# 顯示正在更新的主機名稱 #>
     Write-Host "=============================="
     Write-Host "更新 $hostname 主機"
     Write-Host "=============================="
 
-    # 使用 SSH 命令執行虛擬機更新作業
+    <# 使用 SSH 命令執行虛擬機更新作業 #>
     ssh $hostname 'update-vm'
 
-    # 顯示空行
+    <# 顯示空行 #>
     Write-Host ""
 }
 
-# 初始化成功標誌
+<# 初始化成功標誌 #>
 $success = $true
 
-# 遍歷主機名稱列表並呼叫 Update-VM 函數以執行更新作業，並傳遞更新命令
+<# 遍歷主機名稱列表並呼叫 Update-VM 函數以執行更新作業，並傳遞更新命令 #>
 $updateCommand = "update-vm"  # 替換為實際的更新命令路徑
 $hostnames | ForEach-Object {
     Update-VM -hostname $_ -updateCommand 'update-vm'
-    # 檢查是否有更新錯誤，如果有，將成功標誌設置為 false
+    <# 檢查是否有更新錯誤，如果有，將成功標誌設置為 false #>
     if ($LASTEXITCODE -ne 0) {
         $success = $false
     }
 }
 
-# 顯示完成訊息
+<# 顯示完成訊息 #>
 if ($success) {
     Write-Host "全部更新作業完成"
 } else {
     Write-Host "更新出現錯誤"
 }
 
-# 結束程式，並根據成功標誌設置退出代碼
+<# 結束程式，並根據成功標誌設置退出代碼 #>
 Exit $success

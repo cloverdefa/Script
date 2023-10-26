d6230040bba43311c4ae2c9b366234fe73744f93
DAST-HomePC <jackie@dast.tw>
2023-10-26 22:17:47 +0800

    重構：重構檔案處理和錯誤處理
    - 將 shebang 從 `#!/bin/zsh` 改為 `#!/bin/bash`
    - 將註解從中文翻譯為英文
    - 為更新失敗添加錯誤標誌
    - 使用 `for` 迴圈而不是 `while` 迴圈遍歷伺服器列表
    - 根據更新錯誤狀態顯示不同的訊息

--- update-vm.sh
+++ update-vm.sh
@@ -1,7 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 
-# 定义颜色代码
+# 定義顏色代碼
 RED='\033[0;31m' # 红色
+GREEN='\033[0;32m' # 绿色
 NC='\033[0m'     # 重置颜色
 
 # 建立函數用於執行SSH連接和執行update-vm命令
@@ -26,9 +27,11 @@ function update_vm_on_server {
       echo "在 $server 上執行 update-vm 成功"
     else
       echo "在 $server 上執行 update-vm 失敗"
+      update_error=1  # 標記更新錯誤
     fi
   else
     echo "無法執行 update-vm 因為SSH連接失敗"
+    update_error=1  # 標記更新錯誤
   fi
 }
 
@@ -41,10 +44,17 @@ if [ ! -f "$server_list" ]; then
   exit 1
 fi
 
+# 初始化錯誤標誌
+update_error=0
+
 # 使用迴圈循環對伺服器清單內的每一台機器調用函數執行命令
-while IFS= read -r server; do
-  # 忽略空白行和注释行
-  if [[ -n "$server" && ! "$server" == \#* ]]; then
-    update_vm_on_server "$server"
-  fi
-done < "$server_list"
+for server in $(cat "$server_list"); do
+  update_vm_on_server "$server"
+done
+
+# 根據錯誤情況顯示不同的訊息
+if [ $update_error -eq 0 ]; then
+  echo -e "${GREEN}全部機器更新完成${NC}"
+else
+  echo -e "${RED}更新過程發生錯誤${NC}"
+fi

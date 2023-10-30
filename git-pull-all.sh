3029a7364b9ff14d90b11962c9891fb3fbeb6655
DAST-HomePC <jackie@dast.tw>
2023-10-30 18:00:23 +0800

    重構：為了增加可讀性而重構代碼
    - 刪除未使用的顏色代碼
    - 刪除不必要的註釋
    - 將變量 `$text` 更改為增加可讀性
    
    Signed-off-by: DAST-HomePC <jackie@dast.tw>

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# 定義顏色代碼
-RED='\033[0;31m' # 红色
-GREEN='\033[0;32m' # 绿色
-YELLOW='\033[0;33m' # 黄色 
-NC='\033[0m'     # 重置颜色
-
 # 從 .repositories.list 檔案中讀取儲存庫名稱列表，並過濾掉空白行和註解行
 mapfile -t repos < <(grep -E -v '^\s*(#|$)' "$HOME/.config/list/.repositories.list")
 
@@ -13,7 +7,7 @@ mapfile -t repos < <(grep -E -v '^\s*(#|$)' "$HOME/.config/list/.repositories.li
 function Git-Pull-Repo() {
     local repo_name="$1"
     local repo_path="$HOME/Documents/github/$repo_name" # 使用$HOME環境變數
-    local text="${YELLOW}$repo_name${NC} 拉取遠端資料"
+    local text="$repo_name 拉取遠端資料"
 
     cd "$repo_path" || exit 1 # 切換到存儲庫目錄如果切換失敗，退出程式，並返回結束碼1
 
@@ -27,12 +21,12 @@ function Git-Pull-Repo() {
 
     if git remote update -p && git status -uno | grep -q 'Your branch is behind'; then
         if git pull; then
-            text="$text 完成，存儲庫名稱：${GREEN}$repo_name${NC}"
+            text="$text 完成，存儲庫名稱：$repo_name"
         else
-            text="$text 出現錯誤，存儲庫名稱：${RED}$repo_name${NC}"
+            text="$text 出現錯誤，存儲庫名稱：$repo_name"
         fi
     else
-        text="GitHub 遠端資料庫無變更或本地資料不需要更新，存儲庫名稱：${GREEN}$repo_name${NC}"
+        text="GitHub 遠端資料庫無變更或本地資料不需要更新，存儲庫名稱：$repo_name"
     fi
 
     echo "$text"

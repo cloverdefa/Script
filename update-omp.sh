f493be5459fd7f878cc13289ca0c4ad3c1e755ae
Macbook <jackie@dast.tw>
2023-11-11 12:42:47 +0800

    功能：使用顏色格式改善錯誤和成功訊息
    - 修正顏色代碼定義中的拼寫錯誤
    - 將缺失的伺服器清單檔案的錯誤訊息改為包含顏色格式
    - 將每個伺服器的更新訊息改為包含顏色格式
    - 將無效的更新指令的錯誤訊息改為包含顏色格式
    - 刪除無效的更新指令的多餘錯誤訊息
    - 將每個伺服器更新的成功訊息改為包含顏色格式
    - 將至少一個裝置更新失敗的錯誤訊息改為包含顏色格式
    - 將所有裝置更新的成功訊息改為包含顏色格式
    
    Signed-off-by: Macbook <jackie@dast.tw>

--- update-omp.sh
+++ update-omp.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# 定義顏色代碼
+RED='\033[0;31m' # 红色
+GREEN='\033[0;32m' # 绿色
+YELLOW='\033[0;33m' # 黄色 
+NC='\033[0m'     # 重置颜色
+
 # 讀取伺服器清單文件
 server_list="$HOME/.config/list/.omp.list"
 
 # 檢查清單列表文件是否存在
 if [ ! -f "$server_list" ]; then
-  echo "伺服器清單文件不存在: $server_list"
+  echo -e "${RED}伺服器清單文件不存在: $server_list${NC}"
   exit 1
 fi
 
@@ -13,39 +19,31 @@ fi
 function Update-OMP() {
     local server="$1"
     local update_command="$2"
-    echo "======================================="
-    echo "      更新 $server 主機 oh my posh"
-    echo "======================================="
+    echo -e "${YELLOW}更新 $server 主機 oh my posh${NC}"
     if [ -n "$update_command" ]; then
         # 使用 SSH 命令執行 oh-my-posh 更新作業
-        ssh "$server" "$update_command"
-        local exit_code="$?"
-        if [ "$exit_code" -ne 0 ]; then
-            echo "==== 更新 $server 出現錯誤 ===="
-            exit 1
-        else
-            echo "==== 更新 $server 完成 ===="
-        fi
+        ssh "$server" "$update_command" || {
+            echo -e "${RED}==== 更新 $server 出現錯誤 ====${NC}"
+            return 1
+        }
+        echo -e "${GREEN}==== 更新 $server 完成 ====${NC}"
     else
-        echo "==== 未提供有效的更新命令 ===="
-        exit 1
+        echo -e "${RED}==== 未提供有效的更新命令 ====${NC}"
+        return 1
     fi
 }
 
 # 使用迴圈循環對伺服器清單內的每一台機器調用函數執行命令
 error_occurred=false
 for server in $(grep -v '^\s*#' "$server_list" | grep -v '^\s*$'); do
-  Update-OMP "$server" 'ompu'
-  if [ "$?" -ne 0 ]; then
-    error_occurred=true
-  fi
+    Update-OMP "$server" 'ompu' || error_occurred=true
 done
 
 # 檢查是否發生錯誤
 if [ "$error_occurred" = true ]; then
-    echo "==== 更新至少一個設備出現錯誤 ===="
+    echo -e "${RED}==== 更新至少一個設備出現錯誤 ====${NC}"
 else
-    echo "==== 更新全部設備完成 ===="
+    echo -e "${GREEN}==== 更新全部設備完成 ====${NC}"
 fi
 
 # 結束程式

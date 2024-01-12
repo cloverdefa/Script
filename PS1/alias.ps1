8ea8c5a97400d620d303bcf33eb32940704a185c
DAST-OfficePC <jackie@dast.tw>
2024-01-12 08:13:08 +0800

    特徵: 為不同的用例新增各種 git 函數
    - 為不同的用例新增數個 git checkout 函數
    - 新增具有互動選項的 git clean 函數
    - 新增具有子模組支援的 git clone 函數
    - 新增具有不同選項和標誌的 git commit 函數
    - 新增列出配置設定的 git config 函數
    - 新增取得最近標籤的 git describe 函數

--- PS1/alias.ps1
+++ PS1/alias.ps1
@@ -139,3 +139,119 @@ function ggsup {
 function gbg {
     LANG=C git branch -vv | grep ": gone\]"
 }
+
+function gco {
+    git checkout
+}
+
+function gcor {
+    git checkout --recurse-submodules
+}
+
+function gcb {
+    git checkout -b
+}
+
+function gcB {
+    git checkout -B
+}
+
+function gcd {
+    git checkout $(git_develop_branch)
+}
+
+function gcm {
+    git checkout $(git_main_branch)
+}
+
+function gcp {
+    git cherry-pick
+}
+
+function gcpa {
+    git cherry-pick --abort
+}
+
+function gcpc {
+    git cherry-pick --continue
+}
+
+function gclean {
+    git clean --interactive -d
+}
+
+function gcl {
+    git clone --recurse-submodules
+}
+
+function gccd {
+    git clone --recurse-submodules "$@" && cd "$(basename $\_ .git)"
+}
+
+function gcam {
+    git commit --all --message
+}
+
+function gcas {
+    git commit --all --signoff
+}
+
+function gcasm {
+    git commit --all --signoff --message
+}
+
+function gcmsg {
+    git commit --message
+}
+
+function gcsm {
+    git commit --signoff --message
+}
+
+function gc {
+    git commit --verbose
+}
+
+function gca {
+    git commit --verbose --all
+}
+
+function gca! {
+    git commit --verbose --all --amend
+}
+
+function gcan! {
+    git commit --verbose --all --no-edit --amend
+}
+
+function gcans! {
+    git commit --verbose --all --signoff --no-edit --amend
+}
+
+function gc! {
+    git commit --verbose --amend
+}
+
+function gcn! {
+    git commit --verbose --no-edit --amend
+}
+
+function gcs {
+    git commit -S
+}
+
+function gcss {
+    git commit -S -s
+}
+
+function gcssm {
+    git commit -S -s -m
+}
+
+function gcf {
+    git config --list
+}
+
+function gdct {
+    git describe --tags $(git rev-list --tags --max-count=1)
+}

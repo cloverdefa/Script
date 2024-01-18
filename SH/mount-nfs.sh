fd8feee289edc38f9832fd3ece062d13eab2c9ce
Macbook <jackie@dast.tw>
2024-01-18 20:18:35 +0800

    新增功能：將註解和錯誤訊息改成繁體中文
    - 將註解從英文改成繁體中文
    - 將挂載嘗試次數從3增加到5
    - 將 ping 嘗試次數從5增加到10
    - 當無法挂載伺服器時，將錯誤訊息從英文更新為繁體中文
    
    Signed-off-by: Macbook <jackie@dast.tw>

--- SH/mount-nfs.sh
+++ SH/mount-nfs.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#
-# This script should do:
-#
-# 1) test if nfs folder is mounted and exit 0 if yes
-# 2) try to mount it and exit 0 if success
-# 3) try to wake the server if not possible to mount
-# 4) wait while its not woked (pinging)
-# 5) try againt 2-4 several times
-#
+# 這個腳本的功能包括：
+# 1) 測試 NFS 資料夾是否已掛載，若是則退出並返回 0
+# 2) 嘗試掛載該資料夾，若成功則退出並返回 0
+# 3) 若無法掛載，嘗試喚醒伺服器
+# 4) 等待伺服器喚醒（透過 ping）
+# 5) 重複步驟 2-4 數次#
 
-# settings
+# 設定
 target_ip=192.168.1.10
 target_folder=/home/share
 target_mac=11:22:33:44:55:66
@@ -18,40 +15,40 @@ mount_folder=/mnt/nfs/server
 mount_attempts=3
 ping_attempts=5
 
-#test if its already mounted
+# 測試是否已經掛載：
 if [ -n "`mount | grep $mount_folder`" ]; then
-  echo "Server already mounted."
+  echo "伺服器資料夾已經掛載."
   exit 0
 fi
 
 if [ ! -d "$mount_folder" ]; then
-  echo "Mount point $mount_folder doesn't exist"
+  echo "掛載目錄 $mount_folder 不存在"
   exit 1
 fi
 
-#loop (set number of attempts here)
+# 循環（在此設定嘗試次數）：
 for ((mount_cnt=1; mount_cnt<=$mount_attempts; mount_cnt++))
 do
-  echo -n "Attempt to mount ($mount_cnt/3): "
-  #mount attempt (set server ip and shared folder)
+  echo -n "嘗試掛載資料夾 ($mount_cnt/3): "
+  # 掛載嘗試（設定伺服器 IP 和共享資料夾）：
   mount $target_ip:$target_folder $mount_folder
 
-  #test if it worked
+  # 測試是否成功：
   if [ $? -eq 0 ]; then
-    echo "Server succesfully mounted."
+    echo "伺服器資料夾成功掛載."
     exit 0
   else
-    #test if wakeonlan installed
-    command -v wakeonlan >/dev/null 2>&1 || { echo "Error: wakeonlan not installed .. Aborting." >&2; exit 1; }
-    #try to wake server (set server mac)
+    # 測試是否已安裝 Wake-on-LAN：
+    command -v wakeonlan >/dev/null 2>&1 || { echo "錯誤: wakeonlan 未安裝 .. 中止操作." >&2; exit 1; }
+    # 嘗試喚醒伺服器（設定伺服器的 MAC 地址）：
     wakeonlan $target_mac
-    #ping until not ready
-    echo -n "Ping attempts ."
+    # Ping 直到伺服器就緒：
+    echo -n "Ping 嘗試 ."
     for ((ping_cnt=1; ping_cnt<=$ping_attempts; ping_cnt++))
     do
       ping -c 1 $target_ip > /dev/null
       if [ $? -eq 0 ]; then
-        echo -n " success."
+        echo -n " 成功."
         break
       else
         echo -n "."
@@ -61,5 +58,5 @@ do
   fi
 done
 
-echo "Error: Unable to mount server."
+echo "錯誤: 無法掛載資料夾."
 exit 1

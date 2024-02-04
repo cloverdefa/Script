aa02704c1fcae5992438ccf44c0aadd86f3af418
DAST-HomePC <jackie@dast.tw>
2024-02-04 20:28:40 +0800

    工作：更新套件升級訊息和指令
    - 將「Dist Upgrade Packages」的文字改為「Full Upgrade Packages」，以表示套件的完整升級
    - 新增一行指令「sudo apt-get dist-upgrade」以執行完整套件升級的動作
    
    Signed-off-by: DAST-HomePC <jackie@dast.tw>

--- SH/do-update.sh
+++ SH/do-update.sh
@@ -6,8 +6,8 @@
 { echo -e "\e[5;37;40m Update Packages \e[0m"; } 2> /dev/null
 sudo apt-get update
 
-{ echo -e "\e[5;37;40m Dist Upgrade Packages \e[0m"; } 2> /dev/null
-sudo pveupgrade
+{ echo -e "\e[5;37;40m Full Upgrade Packages \e[0m"; } 2> /dev/null
+sudo apt-get dist-upgrade
 
 { echo -e "\e[5;37;40m Remove Dependency Packages That Are No Longer Needed \e[0m"; } 2> /dev/null
 sudo apt-get --purge autoremove

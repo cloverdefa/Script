92e079926500e0fddcb1951346c1226f1e5ad3a4
OfficePC-WSL <jackie@dast.tw>
2024-03-15 08:54:54 +0800

    重構：重構文件處理和本地化代碼
    - 更新注釋為英文
    - 將遠程版本與本地版本進行比較
    - 重構版本比較邏輯
    - 下載文件並設置執行權限
    - 移動文件到 /usr/local/bin 路徑

--- SH/codegpt-update.sh
+++ SH/codegpt-update.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-# 提示使用者輸入CodeGPT下載連結
+# 提示用户输入 CodeGPT 下载链接
 read -p "請輸入 CodeGPT 下載的連結: " url
 
-# 獲取遠端CodeGPT版本
+# 获取远程文件的版本信息
 remote_version=$(wget -qO- $url | grep -o 'x-amz-meta-version: [0-9.]*' | cut -d ' ' -f 2)
 
-# 獲取本地CodeGPT版本
+# 获取本地文件的版本信息
 local_version=$(codegpt version | awk '{print $3}')
 
-# 比較遠端版本與本地版本
-if (( $(echo "$remote_version > $local_version" | bc -l) )); then
-    # 下載檔案並命名為codegpt
+# 比较远程版本和本地版本
+IFS='.' read -ra remote_parts <<< "$remote_version"
+IFS='.' read -ra local_parts <<< "$local_version"
+update_needed=false
+
+for i in "${!remote_parts[@]}"; do
+    if [[ "${remote_parts[$i]}" -gt "${local_parts[$i]}" ]]; then
+        update_needed=true
+        break
+    elif [[ "${remote_parts[$i]}" -lt "${local_parts[$i]}" ]]; then
+        break
+    fi
+done
+
+if [ "$update_needed" = true ]; then
+    # 下载文件并命名为codegpt
     wget -O codegpt "$url"
 
-    # 賦予可執行權限
+    # 赋予可执行权限
     chmod +x codegpt
 
-    # 將檔案移動到/usr/local/bin路徑下
+    # 将文件移动到/usr/local/bin路径
     sudo mv codegpt /usr/local/bin/
 
     echo "已下载新版本的 CodeGPT，并已移动到 /usr/local/bin 路径下"

e89c448c9dc7cd66d2afe56f144644c5f325e16c
HomePC-WSL <jackie@dast.tw>
2024-03-23 08:57:27 +0800

    重構：在檔案間改善版本比較邏輯
    - 更新版本比較邏輯以處理主要、次要和修訂版本
    - 刪除不必要的註釋和代碼重複

--- SH/codegpt-update.sh
+++ SH/codegpt-update.sh
@@ -1,27 +1,18 @@
-#! /usr/bin/env bash
-
-# 提示使用者輸入 CodeGPT 下載連結
-read -p "請輸入 CodeGPT 下載的連結: " url
-
-# 獲取遠端CodeGPT版本
-remote_version=$(wget -qO- $url | grep -o 'x-amz-meta-version: [0-9.]*' | cut -d ' ' -f 2)
-
-# 獲取本地CodeGPT版本
-local_version=$(codegpt version | awk '{print $3}')
-
 # 比較遠端與本地的版本
 IFS='.' read -ra remote_parts <<< "$remote_version"
 IFS='.' read -ra local_parts <<< "$local_version"
 update_needed=false
 
-for i in "${!remote_parts[@]}"; do
-    if [[ "${remote_parts[$i]}" -gt "${local_parts[$i]}" ]]; then
-        update_needed=true
-        break
-    elif [[ "${remote_parts[$i]}" -lt "${local_parts[$i]}" ]]; then
-        break
-    fi
-done
+# 比較主要版本號
+if [[ "${remote_parts[0]}" -gt "${local_parts[0]}" ]]; then
+    update_needed=true
+# 如果主要版本號相同，則比較次要版本號
+elif [[ "${remote_parts[0]}" -eq "${local_parts[0]}" && "${remote_parts[1]}" -gt "${local_parts[1]}" ]]; then
+    update_needed=true
+# 如果次要版本號也相同，則比較修訂版本號
+elif [[ "${remote_parts[0]}" -eq "${local_parts[0]}" && "${remote_parts[1]}" -eq "${local_parts[1]}" && "${remote_parts[2]}" -gt "${local_parts[2]}" ]]; then
+    update_needed=true
+fi
 
 if [ "$update_needed" = true ]; then
     # 下載檔案並命名為codegpt

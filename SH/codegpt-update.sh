af8c39220bede222e17035f60346134cec5a185a
OfficePC-WSL <jackie@dast.tw>
2024-03-27 15:12:51 +0800

    重構：標準化語言使用和改進腳本可讀性跨檔案
    - 在腳本消息中更新中文文字以使用英文
    - 添加檢查成功擷取遠端和本地版本的功能
    - 更新腳本輸出以使用中英文文字，以提高可讀性

--- SH/codegpt-update.sh
+++ SH/codegpt-update.sh
@@ -3,11 +3,23 @@
 # 提示使用者輸入 CodeGPT 下載連結
 read -p "請輸入 CodeGPT 下載的連結: " url
 
-# 獲取遠端CodeGPT版本
-remote_version=$(wget -qO- $url | grep -o 'x-amz-meta-version: [0-9.]*' | cut -d ' ' -f 2)
+# 獲取遠端 CodeGPT 版本
+remote_version=$(wget -qO- "$url" | grep -o 'x-amz-meta-version: [0-9.]*' | cut -d ' ' -f 2)
 
-# 獲取本地CodeGPT版本
+# 检查远程版本是否成功获取
+if [ -z "$remote_version" ]; then
+    echo "无法获取远程版本号，请检查链接和网络连接。"
+    exit 1
+fi
+
+# 獲取本地 CodeGPT 版本
 local_version=$(codegpt version | awk '{print $3}')
+
+# 检查本地版本是否成功获取
+if [ -z "$local_version" ]; then
+    echo "无法获取本地版本号，请确保 codegpt 命令可用。"
+    exit 1
+fi
 
 # 比較遠端與本地的版本
 IFS='.' read -ra remote_parts <<< "$remote_version"
@@ -25,18 +37,17 @@ elif [ "${remote_parts[0]}" -eq "${local_parts[0]}" ] && [ "${remote_parts[1]}" 
     update_needed=true
 fi
 
-
 if [ "$update_needed" = true ]; then
-    # 下載檔案並命名為codegpt
+    # 下載檔案並命名為 codegpt
     wget -O codegpt "$url"
 
     # 賦予可執行權限
     chmod +x codegpt
 
-    # 將檔案移動到/usr/local/bin路徑下
+    # 將檔案移動到 /usr/local/bin 路徑下
     sudo mv codegpt /usr/local/bin/
 
-    echo "已下载新版本的 CodeGPT，並移動到 /usr/local/bin 路徑下"
+    echo "已下载新版本的 CodeGPT，并移動到 /usr/local/bin 路徑下"
 else
     echo "已存在本地最新版本的 CodeGPT，無須更新"
 fi

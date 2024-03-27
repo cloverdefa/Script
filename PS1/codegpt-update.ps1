0705c0ff2083cd8521bbfb401184a654f23d7b28
OfficePC-WSL <jackie@dast.tw>
2024-03-28 07:58:49 +0800

    功能: 重構版本管理和下載處理
    - 更新用戶提示以輸入版本號碼
    - 從用戶輸入中提取版本號碼和架構
    - 基於版本和架構構建下載鏈接
    - 更新本地版本提取方法
    - 修改版本比較功能參數
    - 更改檢測到新版本和下載確認的輸出消息
    - 調整下載文件保存和移動過程，包括目標路徑和文件名
    - 更新現有本地最新版本的輸出消息和邏輯錯誤修復

--- PS1/codegpt-update.ps1
+++ PS1/codegpt-update.ps1
@@ -1,15 +1,18 @@
-# 提示使用者輸入 CodeGPT 下載連結
-$url = Read-Host "請輸入 CodeGPT 下載的連結"
+# 提示使用者輸入版本號碼
+$input = Read-Host "請輸入 CodeGPT 的版本號碼（例如amd 0.10.0）"
 
-# 獲取遠端檔案的版本信息
-$webRequest = Invoke-WebRequest -Uri $url
-$remoteVersion = $webRequest.Headers['x-amz-meta-version']
+# 從輸入中提取版本號碼和架構
+$arch = ($input -split ' ')[0]
+$version = ($input -split ' ')[1]
 
-# 獲取本地檔案的版本信息
+# 構建下載連結
+$url = "https://github.com/appleboy/CodeGPT/releases/download/v$version/CodeGPT-$version-windows-$arch.exe"
+
+# 獲取本地版本號碼
 $localVersionOutput = & codegpt version
-$localVersion = ($localVersionOutput -split ' ')[1]
+$localVersion = ($localVersionOutput -split ': ')[1]
 
-# 比较版本函数
+# 比較版本函數
 function Compare-Versions {
     param($version1, $version2)
     $version1Parts = $version1 -split '\.'
@@ -25,24 +28,24 @@ function Compare-Versions {
     return 0
 }
 
-# 比较本地版本與遠端版本
-$compareResult = Compare-Versions $remoteVersion $localVersion
+# 比較本地版本與用戶輸入的版本
+$compareResult = Compare-Versions $version $localVersion
 if ($compareResult -gt 0) {
-    Write-Host "已偵測到新版本的 CodeGPT"
-    $confirmation = Read-Host "是否要下载並更新到新版本？ (輸入 'y' 確認)"
+    Write-Host "已檢測到新版本的 CodeGPT"
+    $confirmation = Read-Host "是否要下載並更新到新版本？ (輸入 'y' 確認)"
     if ($confirmation -eq 'y') {
-        # 下载檔案並命名為 codegpt_new.exe
+        # 下載檔案並保存為 codegpt_new.exe
         Invoke-WebRequest -Uri $url -OutFile "codegpt_new.exe"
 
-        # 移動新的檔案到目的地
+        # 移動新檔案到目標位置
         $destinationPath = "$env:USERPROFILE\OneDrive\文件\.bin\codegpt.exe"
-        Move-Item -Path "codegpt_new.exe" -Destination $destinationPath
-        Write-Host "已下载新版本的 CodeGPT，並已移動到 '$destinationPath' 路徑下"
+        Move-Item -Path "codegpt_new.exe" -Destination $destinationPath -Force
+        Write-Host "已下載新版本的 CodeGPT，並已移動到 '$destinationPath' 路徑下"
     } else {
         Write-Host "已取消下載和更新操作"
     }
 } elseif ($compareResult -eq 0) {
-    Write-Host "已存在本地最新版本的 CodeGPT，無須更新"
+    Write-Host "本地已是最新版本的 CodeGPT，無需更新"
 } else {
-    Write-Host "本地版本高於遠端版本，邏輯錯誤"
+    Write-Host "本地版本高於輸入版本，邏輯錯誤"
 }

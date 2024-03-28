ba48fa1143b013884b9e5b49204b95c0bf03b25a
OfficePC-WSL <jackie@dast.tw>
2024-03-28 08:09:03 +0800

    特性: 重構針對架構的訊息和網址
    - 更新提示訊息以適當方式要求版本號
    - 更新架構萃取提示訊息
    - 更新無法萃取版本號的錯誤訊息
    - 根據架構修改URL後綴
    - 根據架構更新下載連結建構
    - 更新下載並更新版本的提示訊息
    - 更新本地版本檢索的訊息
    - 更新比較遠端與本地版本的訊息

--- SH/codegpt-update.sh
+++ SH/codegpt-update.sh
@@ -1,39 +1,49 @@
 #!/usr/bin/env bash
 
-# 提示用户输入版本号码
-read -p "請輸入 CodeGPT 的版本號碼（例如arm 0.10.0）: " input
+# 提示使用者輸入版本號碼
+read -p "請輸入 CodeGPT 的版本號碼（例如amd 0.10.0）: " input
 
-# 从输入中提取版本号码
+# 從輸入中提取版本號碼
 version=$(echo "$input" | awk '{print $2}')
 
-# 检查版本号码是否成功提取
+# 檢查版本號碼是否成功提取
 if [[ -z "$version" || ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "無法從輸入中提取版本號碼，請確保輸入格式正確（例如：arm 0.10.0）。"
+    echo "無法從輸入中提取版本號碼，請確保輸入格式正確（例如：amd 0.10.0）。"
     exit 1
 fi
 
-# 提取架构
+# 提取架構
 arch=$(echo "$input" | awk '{print $1}')
 
 echo "版本號碼為: $version"
 echo "架構為: $arch"
 
-# 组合修改的链接
-url="https://github.com/appleboy/CodeGPT/releases/download/v${version}/CodeGPT-${version}-linux-${arch}64"
+# 根據架構添加後綴
+if [ "$arch" == "amd" ]; then
+    arch_suffix="amd64"
+elif [ "$arch" == "arm" ]; then
+    arch_suffix="arm64"
+else
+    echo "不支援的架構"
+    exit 1
+fi
+
+# 組合修改的連結
+url="https://github.com/appleboy/CodeGPT/releases/download/v${version}/CodeGPT-${version}-linux-${arch_suffix}"
 
 echo "下載連結為: $url"
 
-# 获取本地版本号码
+# 取得本地版本號碼
 local_version=$(codegpt version | awk '{print $2}')
 
 echo "本地版本號碼為: $local_version"
 
-# 比较远端与本地的版本
+# 比較遠端與本地的版本
 if [ "$version" != "$local_version" ]; then
-    # 提示用户是否要下载并更新版本
+    # 提示使用者是否要下載並更新版本
     read -p "檢測到新版本的 CodeGPT，是否要下載並更新到新版本？(Y/N): " choice
     if [[ $choice == "Y" || $choice == "y" ]]; then
-        # 下载文件并移动到 /usr/local/bin
+        # 下載檔案並移動到 /usr/local/bin
         wget -qO codegpt "$url"
         chmod +x codegpt
         sudo mv codegpt /usr/local/bin/

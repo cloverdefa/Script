983ac6e96be0ffccc8af8ca6f292004330e034c4
OfficePC-WSL <jackie@dast.tw>
2024-07-08 11:01:55 +0800

    修正: 重新設計 PowerShell 腳本以提升使用便利性及可讀性
    - 修正 PowerShell 腳本以增進可讀性與功能性
    - 將函數名稱從 `fshow` 更改為 `Show-GitCommits`
    - 更新函數以在 Out-GridView 中顯示提交詳細資訊，以提供更佳的使用者體驗
    
    Signed-off-by: OfficePC-WSL <jackie@dast.tw>

--- Windows/fshow.ps1
+++ Windows/fshow.ps1
@@ -1,14 +1,26 @@
-# Fuzzy search over Git commits
-# Enter will view the commit
-# Ctrl-o will checkout the selected commit
-function fshow() {
-  git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" |
-    fzf --ansi --no-sort --reverse --tiebreak=index --preview \
-      'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
-      --bind "j:down,k:up,alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,q:abort,ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
-FZF-EOF" --preview-window=right:60%
+# PowerShell script to fuzzy search and view Git commits
+
+function Show-GitCommits {
+    # Get the Git commit log with necessary formatting
+    $log = git log --graph --pretty=format:'%h%d %s %cr' --date=relative --color=always
+
+    # Parse the log into an array of objects for Out-GridView
+    $commits = $log -split "`n" | ForEach-Object {
+        if ($_ -match '(\w{7}) (.*)') {
+            [PSCustomObject]@{
+                Hash = $matches[1]
+                Message = $matches[2]
+            }
+        }
+    }
+
+    # Display the commits in Out-GridView for fuzzy searching
+    $selectedCommit = $commits | Out-GridView -Title "Select a Git commit" -PassThru
+
+    if ($null -ne $selectedCommit) {
+        # Show the selected commit details in less
+        git show --color=always $selectedCommit.Hash | less -R
+    }
 }
-fshow "$@"
+
+Show-GitCommits

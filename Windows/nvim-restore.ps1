797fdace0f103f9721080601bd29200f0e0ad81c
HomePC <jackie@dast.tw>
2024-07-24 18:26:24 +0800

    refactor: refactor file handling and function additions
    - Remove deletion of folders `$env:LOCALAPPDATA\nvim` and `$env:LOCALAPPDATA\nvim-data`
    - Add functions `Remove-FolderIfExist` and `Clone-Repo` for folder removal and repository cloning
    - Adjust script to use the newly added functions for handling folders and cloning repositories
    
    Signed-off-by: HomePC <jackie@dast.tw>

--- Windows/nvim-restore.ps1
+++ Windows/nvim-restore.ps1
@@ -1,5 +1,35 @@
 <# 重置nvim設定並下載github遠端最新版本設定 #>
-rm -Force "$env:LOCALAPPDATA\nvim"
-rm -Force "$env:LOCALAPPDATA\nvim-data"
 
-git clone https://github.com/cloverdefa/nvim-win "$env:LOCALAPPDATA\nvim"
+$localAppDataNvim = "$env:LOCALAPPDATA\nvim"
+$localAppDataNvimData = "$env:LOCALAPPDATA\nvim-data"
+
+function Remove-FolderIfExist {
+    param (
+        [string]$folderPath
+    )
+    if (Test-Path $folderPath) {
+        Remove-Item -Recurse -Force $folderPath -ErrorAction Stop
+        Write-Host "已刪除資料夾: $folderPath"
+    } else {
+        Write-Host "資料夾不存在: $folderPath"
+    }
+}
+
+function Clone-Repo {
+    param (
+        [string]$repoUrl,
+        [string]$destinationPath
+    )
+    try {
+        git clone $repoUrl $destinationPath -ErrorAction Stop
+        Write-Host "成功克隆 $repoUrl 到 $destinationPath"
+    } catch {
+        Write-Host "克隆過程發生錯誤: $_"
+    }
+}
+
+Remove-FolderIfExist -folderPath $localAppDataNvim
+Remove-FolderIfExist -folderPath $localAppDataNvimData
+
+Clone-Repo -repoUrl "https://github.com/cloverdefa/nvim-win" -destinationPath $localAppDataNvim
+

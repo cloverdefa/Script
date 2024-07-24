1d077b441af08e79eccdcee6642beddf030ed334
HomePC <jackie@dast.tw>
2024-07-24 18:50:32 +0800

    refactor: refactor folder handling and error messages
    - Update comments and variable names in nvim-restore.ps1
    - Refactor folder existence checks and handling in nvim-restore.ps1
    - Adjust error messages for folder deletion and git clone in nvim-restore.ps1
    - Improve conditional logic for git clone execution in nvim-restore.ps1
    
    Signed-off-by: HomePC <jackie@dast.tw>

--- Windows/nvim-restore.ps1
+++ Windows/nvim-restore.ps1
@@ -1,35 +1,31 @@
-<# 重置nvim設定並下載github遠端最新版本設定 #>
+# 設定資料夾路徑
+$nvimFolderPath = "$env:LOCALAPPDATA\nvim"
+$nvimDataFolderPath = "$env:LOCALAPPDATA\nvim-data"
 
-$localAppDataNvim = "$env:LOCALAPPDATA\nvim"
-$localAppDataNvimData = "$env:LOCALAPPDATA\nvim-data"
+# 檢查資料夾是否存在
+$nvimFolderExists = Test-Path -Path $nvimFolderPath -PathType Container
+$nvimDataFolderExists = Test-Path -Path $nvimDataFolderPath -PathType Container
 
-function Remove-FolderIfExist {
-    param (
-        [string]$folderPath
-    )
-    if (Test-Path $folderPath) {
-        Remove-Item -Recurse -Force $folderPath -ErrorAction Stop
-        Write-Host "已刪除資料夾: $folderPath"
-    } else {
-        Write-Host "資料夾不存在: $folderPath"
-    }
-}
-
-function Clone-Repo {
-    param (
-        [string]$repoUrl,
-        [string]$destinationPath
-    )
+# 刪除資料夾
+if ($nvimFolderExists -and $nvimDataFolderExists) {
     try {
-        git clone $repoUrl $destinationPath -ErrorAction Stop
-        Write-Host "成功克隆 $repoUrl 到 $destinationPath"
+        Remove-Item -Path $nvimFolderPath -Recurse -Force
+        Remove-Item -Path $nvimDataFolderPath -Recurse -Force
+        Write-Output "資料夾已成功刪除。"
+        
+        # 執行 git clone 指令
+        git clone https://github.com/cloverdefa/nvim-win.git $env:LOCALAPPDATA\nvim
+        Write-Output "git clone 指令已成功執行。"
     } catch {
-        Write-Host "克隆過程發生錯誤: $_"
+        Write-Output "刪除資料夾或執行 git clone 時發生錯誤：$_"
     }
+} else {
+    if (-not $nvimFolderExists) {
+        Write-Output "$nvimFolderPath 資料夾不存在。"
+    }
+    if (-not $nvimDataFolderExists) {
+        Write-Output "$nvimDataFolderPath 資料夾不存在。"
+    }
+    Write-Output "不執行 git clone 指令。"
 }
-
-Remove-FolderIfExist -folderPath $localAppDataNvim
-Remove-FolderIfExist -folderPath $localAppDataNvimData
-
-Clone-Repo -repoUrl "https://github.com/cloverdefa/nvim-win" -destinationPath $localAppDataNvim
 

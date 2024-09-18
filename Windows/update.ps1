ba608ead43a4646945e78e09839e21dccbb66682
HomePC <jackie@dast.tw>
2024-09-18 21:47:30 +0800

    refactor: refactor file paths for Windows compatibility
    - Update file paths to use `$env:USERPROFILE` instead of `$HOME`
    - Adjust paths in scripts for better compatibility with Windows environments
    
    Signed-off-by: HomePC <jackie@dast.tw>

--- Windows/update.ps1
+++ Windows/update.ps1
@@ -1,8 +1,8 @@
 # 更新 git repo
 # 檢查系統內是否安裝 git 並且在路徑 $HOME\github\Script\Windows 下是否有 git-pull-all.ps1 這個腳本
 if (Get-Command git -ErrorAction SilentlyContinue) {
-    if (Test-Path "$HOME\github\Script\Windows\git-pull-all.ps1") {
-        . "$HOME\github\Script\Windows\git-pull-all.ps1"
+    if (Test-Path "$env:USERPROFILE\github\Script\Windows\git-pull-all.ps1") {
+        . "$env:USERPROFILE\github\Script\Windows\git-pull-all.ps1"
     } else {
         Write-Host "git-pull-all.ps1 腳本未找到"
     }
@@ -17,7 +17,7 @@ winget update --all
 wsl --update
 
 # 檢查是否安裝 pip 在指定的路徑
-$pipPath = Join-Path $HOME "AppData\Local\Programs\Python\Python312\Scripts\pip.exe"
+$pipPath = Join-Path "$env:USERPROFILE\AppData\Local\Programs\Python\Python312\Scripts\pip.exe"
 if (Test-Path $pipPath) {
     & $pipPath install --upgrade debugpy hererocks pip pynvim
 } else {
@@ -27,8 +27,8 @@ if (Test-Path $pipPath) {
 # 檢查更新 CodeGPT
 # 檢查是否有安裝 codegpt 並且在路徑 $HOME\github\Script\Windows 下是否有 codegpt-update.ps1 這個腳本
 if (Get-Command codegpt -ErrorAction SilentlyContinue) {
-    if (Test-Path "$HOME\github\Script\Windows\codegpt-update.ps1") {
-        . "$HOME\github\Script\Windows\codegpt-update.ps1"
+    if (Test-Path "$env:USERPROFILE\github\Script\Windows\codegpt-update.ps1") {
+        . "$env:USERPROFILE\github\Script\Windows\codegpt-update.ps1"
     } else {
         Write-Host "codegpt-update.ps1 腳本未找到"
     }
@@ -39,8 +39,8 @@ if (Get-Command codegpt -ErrorAction SilentlyContinue) {
 # 檢查更新 yt-dlp
 # 檢查是否有安裝 yt-dlp 並且在路徑 $HOME\github\Script\Windows 下是否有 yt-dlp-update.ps1 這個腳本
 if (Get-Command yt-dlp -ErrorAction SilentlyContinue) {
-    if (Test-Path "$HOME\github\Script\Windows\yt-dlp-update.ps1") {
-        . "$HOME\github\Script\Windows\yt-dlp-update.ps1"
+    if (Test-Path "$env:USERPROFILE\github\Script\Windows\yt-dlp-update.ps1") {
+        . "$env:USERPROFILE\github\Script\Windows\yt-dlp-update.ps1"
     } else {
         Write-Host "yt-dlp-update.ps1 腳本未找到"
     }

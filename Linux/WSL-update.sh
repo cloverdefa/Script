8e8c46fe6c348ea3d5ec7b57a28c227cc20c7f60
Macbook <jackie@dast.tw>
2024-09-23 22:19:20 +0800

    chore: improve directory synchronization and upgrade process
    - Check if `git` is installed before syncing directories
    - Add synchronization function for directories with option to use `sudo`
    - Perform synchronization for user directories without sudo
    - Perform synchronization for root directories with sudo
    - Upgrading `Oh My Posh` if it is installed
    
    Signed-off-by: Macbook <jackie@dast.tw>

--- Linux/WSL-update.sh
+++ Linux/WSL-update.sh
@@ -1,5 +1,47 @@
 #!/usr/bin/env bash
 
+if ! command -v git &>/dev/null; then
+  echo "請檢查 GIT 是否安裝"
+  exit 1
+fi
+
+# 定義要檢查的目錄
+user_directories=(
+  "$HOME/.fzf"
+  "$HOME/.fzf-git.sh"
+  "$HOME/.pyenv"
+  "$HOME/.nvm"
+)
+root_directories=(
+  "/root/.fzf"
+  "/root/.fzf-git.sh"
+  "/root/.pyenv"
+  "/root/.nvm"
+)
+# 定義函數來同步目錄
+sync_directories() {
+  local use_sudo=$1
+  shift
+  local directories=("$@")
+  for dir in "${directories[@]}"; do
+    if [ "$use_sudo" = true ]; then
+      if sudo [ -d "$dir" ]; then
+        echo "同步目錄: $dir"
+        sudo bash -c "cd '$dir' && git pull"
+      fi
+    else
+      if [ -d "$dir" ]; then
+        echo "同步目錄: $dir"
+        cd "$dir" && git pull
+      fi
+    fi
+  done
+}
+# 同步用戶目錄
+sync_directories false "${user_directories[@]}"
+# 同步 root 目錄
+sync_directories true "${root_directories[@]}"
+
 # 更新 Oh My Posh
 if command -v oh-my-posh &>/dev/null; then
   oh-my-posh upgrade

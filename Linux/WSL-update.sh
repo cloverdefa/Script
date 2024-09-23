71445e0cc2bf219a8fd845b87b98db0f5389f431
Macbook <jackie@dast.tw>
2024-09-23 22:11:04 +0800

    chore: update `Oh My Posh` installation process
    - Remove checks for `git` installation
    - Update `Oh My Posh` using `oh-my-posh upgrade`
    - Skip updating `Oh My Posh` if not installed or configured correctly
    
    Signed-off-by: Macbook <jackie@dast.tw>

--- Linux/WSL-update.sh
+++ Linux/WSL-update.sh
@@ -1,52 +1,11 @@
 #!/usr/bin/env bash
 
-# 檢查是否安裝了 git
-if ! command -v git &>/dev/null; then
-  echo "請檢查 GIT 是否安裝"
-  exit 1
+# 更新 Oh My Posh
+if command -v oh-my-posh &>/dev/null; then
+  oh-my-posh upgrade
+else
+  echo "Oh My Posh未安裝或正確設定,因此略過更新"
 fi
-
-# 定義要檢查的目錄
-user_directories=(
-  "$HOME/.fzf"
-  "$HOME/.fzf-git.sh"
-  "$HOME/.pyenv"
-  "$HOME/.nvm"
-)
-
-root_directories=(
-  "/root/.fzf"
-  "/root/.fzf-git.sh"
-  "/root/.pyenv"
-  "/root/.nvm"
-)
-
-# 定義函數來同步目錄
-sync_directories() {
-  local use_sudo=$1
-  shift
-  local directories=("$@")
-
-  for dir in "${directories[@]}"; do
-    if [ "$use_sudo" = true ]; then
-      if sudo [ -d "$dir" ]; then
-        echo "同步目錄: $dir"
-        sudo bash -c "cd '$dir' && git pull"
-      fi
-    else
-      if [ -d "$dir" ]; then
-        echo "同步目錄: $dir"
-        cd "$dir" && git pull
-      fi
-    fi
-  done
-}
-
-# 同步用戶目錄
-sync_directories false "${user_directories[@]}"
-
-# 同步 root 目錄
-sync_directories true "${root_directories[@]}"
 
 # 更新CodeGPT
 if command -v codegpt &>/dev/null; then
